23a4b1a61497646476829156bf15116fff9f3c54
Andrey <yuryev@yandex.ru>
2023-12-19 23:19:06 +0300

    django проект Homework3 migrate

--- main/models.py
+++ main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField()
+
+    def __str__(self):
+        return f'Категория: {self.name} - {self.description}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField()
+    preview = models.ImageField(upload_to='products/', verbose_name='изображение', null=True, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    price = models.DecimalField(decimal_places=2, max_digits=16)
+    created_at = models.DateTimeField(auto_now_add=True)
+    changed_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Продукт: {self.name} по цене {self.price}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
