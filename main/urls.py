e1252a5d8ce23e7ff542c9fb2840a5d4eb9a74e7
Andrey <yuryev@yandex.ru>
2023-12-24 15:05:46 +0300

    homework 4 task 1

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from main.views import index, contact
+from main.views import index, contact, product
 
 urlpatterns = [
     path('', index),
-    path('contact', contact)
+    path('contact', contact),
+    path('product', product)
 ]
--- main/views.py
+++ main/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render
 from main.models import Product, Category, Contact
 
+
 # Create your views here.
 def index(request):
     product_list = Product.objects.all()
     products = []
-    for indx in range(product_list.count()-1,product_list.count() - 5,-1):
+    for indx in range(product_list.count() - 1, product_list.count() - 5, -1):
         products.append(product_list[indx])
     return render(request, 'main/index.html', context={'products': products})
 
 
 def contact(request):
-
     if request.method == 'POST':
         name = request.POST.get('name')
         email = request.POST.get('email')
@@ -24,3 +24,10 @@ def contact(request):
             return render(request, 'main/contact.html', context={'name': name, 'email': email, 'contacts': contacts})
     contacts = Contact.objects.all()
     return render(request, 'main/contact.html', context={'contacts': contacts})
+
+
+def product(request):
+    product_id = 1
+    product = Product.objects.get(id=product_id)
+    product_info = {'product': product}
+    return render(request, 'main/product.html', context=product_info)
