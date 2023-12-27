a8c811fc512344535318ff79d807d378fee9cf55
Andrey <yuryev@yandex.ru>
2023-12-27 23:06:56 +0300

    Homework 5/Task 3 модификация обработки запросов

--- blog_app/models.py
+++ blog_app/models.py
@@ -3,11 +3,11 @@ from django.db import models
 
 # Create your models here.
 class BlogRecord(models.Model):
-    title = models.CharField(max_length=100)
-    text = models.TextField(null=True,blank=True)
+    title = models.CharField(max_length=150, verbose_name='название')
+    text = models.TextField(null=True,blank=True, verbose_name='содержимое')
     preview = models.ImageField(upload_to='blogs/', verbose_name='изображение', null=True, blank=True)
-    published = models.BooleanField(default=False)
-    viewed = models.IntegerField(blank=False, null=True)
+    is_published = models.BooleanField(default=True, verbose_name='опубликовано')
+    viewed = models.IntegerField(default=0, verbose_name='просмотры')
     created_at = models.DateTimeField(auto_now_add=True)
     slug = models.CharField(max_length=100, null=True, blank=True, verbose_name='slug')
 
--- blog_app/views.py
+++ blog_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from blog_app.models import BlogRecord
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from pytils.translit import slugify
 
 
 # Create your views here.
@@ -10,10 +11,21 @@ class BlogListView(ListView):
     extra_context = {'title': 'Стартовая страница блога'}
     paginate_by = 5
 
+    def get_queryset(self, *args, **kwargs):
+        queryset = super().get_queryset(*args, **kwargs)
+        queryset = queryset.filter(is_published=True)
+        return queryset
+
 
 class BlogDetailView(DetailView):
     model = BlogRecord
     extra_context = {'title': 'Статья детально'}
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.viewed += 1
+        self.object.save()
+        return self.object
 
 
 class BlogCreateView(CreateView):
@@ -22,9 +34,19 @@ class BlogCreateView(CreateView):
     extra_context = {'title': 'Добавление статьи', 'header': 'Форма для создания статьи', 'action': 'Добавить'}
     success_url = reverse_lazy('blog_app:index')
 
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
 
 class BlogUpdateView(UpdateView):
     model = BlogRecord
-    fields = ('text',)
+    fields = ('text', 'is_published')
     extra_context = {'title': 'Изменение статьи', 'header': 'Форма для изменения текста статьи', 'action': 'Изменить'}
     success_url = reverse_lazy('blog_app:index')
+
+    def get_success_url(self):
+        return reverse('blog_app:blog', args=[self.kwargs.get('pk')])
